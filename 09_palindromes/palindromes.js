b17500b99ddc6b331fd8032294f147e746c13d46
Andres Correa <andrescorreac@gmail.com>
2023-09-19 18:42:28 -0500

    Soluciona ejercicio numero 9 https://www.theodinproject.com/lessons/foundations-fundamentals-part-5

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    test1 = text.toLowerCase().replaceAll(/[^a-z0-9]+/g, "")
+    test2 = text.toLowerCase().replaceAll(/[^a-z0-9]+/g, "").split("").reverse().join("")
+    return (test1 === test2) ? true: false;
+    }
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
