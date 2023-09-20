3fb706826736419d48a0736db26e411b51b51646
Andres Correa <andrescorreac@gmail.com>
2023-09-20 08:11:32 -0500

    Soluciona ejercicio numero 12 https://www.theodinproject.com/lessons/foundations-fundamentals-part-5

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((ac, actual)=>{
+        let today = new Date()
+        if (!actual.yearOfDeath) {actual.yearOfDeath = today.getFullYear()}
+        age = actual.yearOfDeath - actual.yearOfBirth
+        acAge = ac.yearOfDeath - ac.yearOfBirth
+        if (age > acAge){
+        ac = actual
+        }
+        return ac
+    },people[0])
 };
 
 // Do not edit below this line
