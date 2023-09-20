a7a09b64f208d2dd5e7dbe1db1974e2d373f8b27
Andres Correa <andrescorreac@gmail.com>
2023-09-19 19:25:02 -0500

    Soluciona ejercicio numero 10 https://www.theodinproject.com/lessons/foundations-fundamentals-part-5

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {return "OOPS"}
+    if (num == String) {+num}
+    let arrayT = [1,1]
+    for (let i = 0; i < num; i++) {
+        arrayT.push((arrayT[i] + arrayT[i+1]))
+    }
+    return arrayT[num-1]
 };
+
+// console.log(array)
+
+//  F{n} = F{n-1} + F{n-2} 
+
 
 // Do not edit below this line
 module.exports = fibonacci;
