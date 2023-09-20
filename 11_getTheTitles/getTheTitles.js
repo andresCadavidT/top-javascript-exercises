e5e941426d2cdee170eadd3382474f9de61e1282
Andres Correa <andrescorreac@gmail.com>
2023-09-20 07:00:04 -0500

    Soluciona ejercicio numero 11 https://www.theodinproject.com/lessons/foundations-fundamentals-part-5

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((ac, actual) => {
+        return ac = [...ac, actual.title]
+    },[])
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
